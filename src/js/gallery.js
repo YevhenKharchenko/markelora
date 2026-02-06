ecf4207202ee20ccbf74d8527971107272754f11
YevhenKharchenko <evgenvalerych@gmail.com>
2026-02-06 23:38:32 +0200

    Add about, advantages, contact, features, gallery, play and uniqueness sections

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -10,13 +10,13 @@ gallerySwiper = new Swiper('.gallery-swiper-container', {
   slidesPerView: 1,
   slidesPerGroup: 1,
   initialSlide: 0,
-  spaceBetween: 24,
+  spaceBetween: 32,
   speed: 500,
   allowTouchMove: true,
   grabCursor: true,
   breakpoints: {
     1440: {
-      slidesPerView: 4,
+      slidesPerView: 3,
       centeredSlides: false,
       spaceBetween: 32,
       allowTouchMove: false,
